b73f55465c55e3e8a9bfce0aa08b760612582f24
deepa-mallik <57281904+deepa-mallik@users.noreply.github.com>
2019-11-02 12:23:49 +0530

    Add files via upload

---
+++ src/src/Login.java
@@ -0,0 +1,37 @@
+package src;
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import javax.servlet.annotation.*;
+@WebServlet("/Login")
+public class Login extends HttpServlet
+{	public void service(HttpServletRequest req,HttpServletResponse res)
+			throws ServletException,IOException
+	{		try
+		{	res.setContentType("text/html");
+			PrintWriter pw=res.getWriter();
+			String a=req.getParameter("t1");
+			String b=req.getParameter("t2");
+			Configuration cfg=new Configuration();
+			SessionFactory sf=cfg.configure().buildSessionFactory();
+			Session ss=sf.openSession();
+			mypojo pojo=new mypojo();
+			pojo.setUname(a);
+			pojo.setPword(b);
+			Transaction tx=ss.beginTransaction();
+			ss.save(pojo);
+			tx.commit();
+			ss.close();
+			res.sendRedirect("success.html");
+		}
+		catch(Exception ae)
+		{		}	}}
+
+
